a59c05f8228778f79b889ec52e01314409623e2c
John Barbery <johnfbarbery@gmail.com>
2025-03-14 20:55:25 -0400

    Added hash struct def

--- chash.c
+++ chash.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+typedef struct hash_struct
+{
+	// Hash value produced by running the name text through the hash function
+	uint32_t hash;
+	// Full name of person
+	char name[50];
+	// Salary of person
+	uint32_t salary;
+	struct hash_struct *next;
+} hashRecord;
+
 int main(void)
 {
 	printf("Hello, World!\n");
