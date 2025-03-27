ba84ce466cf421abb61b67dd68240cc7028c994c
John Barbery <johnfbarbery@gmail.com>
2025-03-27 18:28:21 -0400

    Added hash table operations (insert, delete, search), and added base code to open the commands text file

--- chash.c
+++ chash.c
@@ -1,18 +1,49 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <unistd.h>
 
 typedef struct hash_struct
 {
 	// Hash value produced by running the name text through the hash function
 	uint32_t hash;
-	// Full name of person
+	// Full name of person (key)
 	char name[50];
-	// Salary of person
+	// Salary of person (value)
 	uint32_t salary;
 	struct hash_struct *next;
 } hashRecord;
 
+void insert(char* key, uint32_t value);
+void delete(char* key);
+void search(char* key);
+
 int main(void)
 {
 	printf("Hello, World!\n");
+	
+	int fd = open("commands.txt", O_RDONLY);
+	printf("Successfully opened the file.\n");
+	
+	close(fd);
+	printf("Successfully closed the file.\n");
+	
 	return 0;
+}
+
+void insert(char* key, uint32_t value)
+{
+	
+}
+
+void delete(char* key)
+{
+	
+}
+
+void search(char* key)
+{
+	
 }
